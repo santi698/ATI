5cc517cdb682069f0f211ff9f6d8134c2feb818b
Santiago Ocamica <santi6982@gmail.com>
2015-05-05 19:12:41 -0300

    Pequeño refactoring a Hough.

--- src/core/hough/Hough.java
+++ src/core/hough/Hough.java
@@ -3,8 +3,8 @@ package core.hough;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.function.BiFunction;
 import java.util.function.BiPredicate;
+import java.util.function.Function;
 
 import org.opencv.core.Mat;
 
@@ -12,7 +12,7 @@ import javafx.geometry.Point2D;
 
 public class Hough {
 	HashMap<Point2D, HoughResults> results;
-	private BiFunction<Double, Double, Double> f;
+	private Function<Point2D, Double> f;
 	private double minX;
 	private double maxX;
 	private double minY;
@@ -22,7 +22,7 @@ public class Hough {
 	private double EPS;
 	private int maxVotes = 0;
 	
-	public Hough(BiFunction<Double, Double, Double> f, double minX, double maxX, double xStep, double minY, double maxY, double yStep) {
+	public Hough(Function<Point2D, Double> f, double minX, double maxX, double xStep, double minY, double maxY, double yStep) {
 		this.f = f;
 		this.minX = minX;
 		this.maxX = maxX;
@@ -38,28 +38,33 @@ public class Hough {
 		
 		for (int i = 0; i < img.width(); i++) {
 			for (int j = 0; j < img.height(); j++) {
-				for (double x = minX; x < maxX; x+=xStep) {
-					for(double y = minY; y < maxY; y+=yStep) {
-						Point2D point = new Point2D(x, y);
-						double color = img.get(j, i)[0];
-						if (color < 0.5) {
-							if (f.apply(x, y) - color < EPS) {
-								if (!results.containsKey(point))
-									results.put(point, new HoughResults());
-								results.get(point).vote(new Point2D(i,j));
-								if (results.get(point).getVotes() > maxVotes)
-									maxVotes = results.get(point).getVotes();
-							}
-						}
+				double color = img.get(j, i)[0];
+				Point2D pixelPosition = new Point2D(i,j);
+				processPixel(color, pixelPosition);
+			}
+		}
+	}
+
+	private void processPixel(double color, Point2D pixelPosition) {
+		for (double x = minX; x < maxX; x+=xStep) {
+			for(double y = minY; y < maxY; y+=yStep) {
+				Point2D point = new Point2D(x, y);
+				
+				if (color < 0.5) {
+					if (f.apply(point) - color < EPS) {
+						if (!results.containsKey(point))
+							results.put(point, new HoughResults());
+						results.get(point).vote(pixelPosition);
+						if (results.get(point).getVotes() > maxVotes)
+							maxVotes = results.get(point).getVotes();
 					}
-						
 				}
 			}
 		}
 	}
 	public List<Point2D> getDetected (BiPredicate<HoughResults, Integer> condition) {
 		List<Point2D> passingTuples = new LinkedList<Point2D>();
-		results.forEach((point,result)-> {
+		results.forEach( (point,result)-> {
 			if (condition.test(result, maxVotes))
 				passingTuples.add(point);
 		});
